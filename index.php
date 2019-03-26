eded4ce6668f7ecf6fd37cfbfc4c530ba4ab58d9
celinelin <842216418@qq.com>
2019-03-25 18:31:35 -0700

    First commit PHEW

---
+++ index.php
@@ -0,0 +1,81 @@
+<?php include("header.php") ?>
+<div id="header" class="container-fluid d-flex">
+	 <div class="menu-overlay"></div>
+	 <div class="menu">
+			<div class="toggler">
+				 <div class="bar1"></div>
+				 <div class="bar2"></div>
+				 <div class="bar3"></div>
+			</div>
+			<ul class="nav-content" id="navigation">
+				 <li><a class="nav-link" href="#header">Home</a></li>
+				 <li><a class="nav-link" href="#about">About</a></li>
+				 <li><a class="nav-link"
+						href="#work">Work</a></li>
+				 <li><a class="nav-link" href="#contact">Contact</a></li>
+			</ul>
+	 </div>
+	 <div class="row align-items-center justify-content-center text-center">
+			<div>
+				 <h1>Alvin Lustig</h1>
+				 <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="124.128 52.761 444.171 569.812" width="" height="700">
+						<defs>
+							 <path d="M391.79 563.16l-11.96-17.09 17.95-35.9-26.5 11.11-13.67 17.1 22.22 24.78 30.77 9.4 29.06 43.59h-41.88l-40.17-15.38-48.72 3.42h-75.21l-2.57 11.96H125.13l5.98-26.49 22.22-35.9 26.5-24.79L220 496.5l28.21-20.52 41.02-19.66 53.7-11.11 48.86 18.81 37.61 17.95-9.4 11.96-62.39-14.53-31.63-3.42-23.93 14.53-35.04 8.55-7.69 15.38h36.75l52.99-15.38 38.46-5.13L420 505.04l2.56 13.68 4.28 57.26 22.22 40.17 116.24 3.42-30.77-38.46-28.21-46.15-36.75-65.81-29.91-20.52-44.45 4.28-37.6-13.68H320l-52.99 17.09-33.33 10.26L220 452.91l47.01-25.65 75.92-17.94 39.46 9.4 28.21 13.67 42.73 12.82 30.77 11.11 15.39-17.09-22.23-11.97-32.47-13.67-34.19-12.82-36.75-5.98-22.23 8.54-22.22 5.99-52.99 9.4 12.82-15.39 46.15-24.78 49.58 9.4 41.88 9.4h33.33l17.09 5.98h37.61l8.55-18.8-28.21-9.4-50.42-9.4-36.76-4.28-34.18 23.08 17.94-69.23-25.64 6.84-20.51 2.56-10.26-16.24 16.24-9.4 24.79-4.27 26.5 11.11L420 281.97l-37.61-10.26-18.8 5.98-30.77 10.26-6.84 6.84-5.98 32.47-6.84 34.19 6.84 19.66-17.95 13.68-22.22 5.98-3.42-19.66 12.82-15.38v-35.05l-5.13-18.8-13.67 3.42-22.22 4.7-23.94-4.7-7.69-12.82 35.04-11.97 28.21 11.97v-14.53l-28.21-10.26-20.51-11.11-33.33 15.39-13.68-7.7 10.26-26.49v-48.72l33.33-45.3h42.74L313.16 170l38.46-23.08 35.9-11.11 35.04 22.22 9.41 35.9 7.69 49.57 9.4 47.01 4.27 24.79v41.02l23.93 11.97 10.26-11.97 11.97-41.02-4.28-33.33-11.11-7.7 3.42-21.36-13.67-68.38-24.79-54.7-41.03-47.86-25.64-9.41-18.8-11.96-37.61-6.84-32.48 2.56-40.17 11.97-29.06 20.51-26.49 30.77-13.68 22.22L163.59 170l5.13 17.09-15.39 19.66 15.39 25.64h-10.26v22.23l15.39 14.53 10.25 39.31" id="a"/>
+						</defs>
+						<use xlink:href="#a" fill-opacity="0" stroke="white"/>
+				 </svg>
+			</div>
+	 </div>
+</div>
+<div id="about">
+	 <div class="container">
+			<header>
+				<img src="http://adcglobal.org/wp-content/uploads/2014/03/alvin-lustig.png" alt="picture">
+			</header>
+	 </div>
+	 
+	 <div class="col-md-9 col-sm-12 offset-md-1">
+			<nav>
+				 <h2>About</h2>
+			</nav>
+			<div id="footer">
+				 <p>Alvin Lustig (February 8, 1915 - 
+						December 5, 1955) was an American 
+						book designer,[1] graphic designer 
+						and typeface designer. Lustig has 
+						been honored by the American 
+						Institute of Graphic Arts and the Art 
+						Directors Club Hall of Fame for his 
+						significant contributions to American 
+						design.
+				 </p>
+			</div>
+	 </div>
+</div>
+
+<div class="content">
+	 <div class="circle blue"></div>
+	 <div class="circle yellow"></div>
+	 <div class="circle red"></div>
+</div>
+<div id="work">
+	 <div class="container">
+			<h3>Work</h3>
+	 </div>
+	 <div class="Graphic Design">
+			<h4>Graphic Design</h4>
+			<a><img src="https://d39a3h63xew422.cloudfront.net/wp-content/uploads/2014/12/22141506/alvin-lustig-was-the-original-design-strategist-1476934003835-476x640.jpg"></a>
+			<h5 class="project-title">Project name</h5>
+			<b><img src="https://d39a3h63xew422.cloudfront.net/wp-content/uploads/2014/12/22153349/alvin-lustig-was-the-original-design-strategist-1476933948240-463x640.jpg"></b>
+			<h5 class="project-title">Project name</h5>
+			<c><img src="https://d39a3h63xew422.cloudfront.net/wp-content/uploads/2014/12/22135228/alvin-lustig-was-the-original-design-strategist-1476934014045-471x640.jpg"></c>
+			<h5 class="project-title">Project name</h5>
+			<d><img src="https://d39a3h63xew422.cloudfront.net/wp-content/uploads/2014/12/22131059/alvin-lustig-was-the-original-design-strategist-1476934031747-483x640.jpg"></d>
+			<h5 class="project-title">Project name</h5>
+			<e><img src="https://d39a3h63xew422.cloudfront.net/wp-content/uploads/2014/12/22125137/alvin-lustig-was-the-original-design-strategist-1476934038129-429x640.jpg"></e>
+			<h5 class="project-title">Project name</h5>
+			<f><img src="https://d39a3h63xew422.cloudfront.net/wp-content/uploads/2014/12/22133131/alvin-lustig-was-the-original-design-strategist-1476934023207-449x640.jpg"></f>
+			<h5 class="project-title">Project name</h5>
+	 </div>
+</div>
+<?php include("footer.php") ?>
