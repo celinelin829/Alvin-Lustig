83520470253fcaf66d1d97c4015d8cf78fe0e6f0
celinelin <842216418@qq.com>
2019-04-10 19:50:04 -0700

    color and footer

--- footer.php
+++ footer.php
@@ -9,13 +9,13 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script> <!-- Adding PrefixFree for you -->
 
 	<!-- Local JS -->
-	<script src="<?php echo '//'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']); ?>/JS/main.js"></script> <!-- Your local Javascript file -->
+	<script src="<?php echo '//'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']); ?>js/main.js"></script> <!-- Your local Javascript file -->
 
 	<!-- CDN CSS Dependencies -->
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"> <!-- Adding Normalize for you -->
 
 	<!-- Local CSS -->
-	<link rel="stylesheet" href="<?php echo '//'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']); ?>/css/main.css"> <!-- Adding Normalize for you -->
+	<link rel="stylesheet" href="<?php echo '//'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']); ?>css/main.css"> <!-- Adding Normalize for you -->
 
 	</body><!-- visible content should end -->
 </html>
